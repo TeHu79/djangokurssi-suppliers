c22a8f878f8d430863ddad157b47c41f8ae8d532
Teija Hurme <teija.hurme@student.careeria.fi>
2025-05-17 10:01:32 +0300

    listaukset ja lisäykset toimii

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import landingview, productlistview, supplierlistview
+from .views import landingview, productlistview, supplierlistview, addsupplier, addproduct
 
 urlpatterns = [
     path('', landingview),
 
     # Products url´s
     path('products/', productlistview),
+    path('add-product/', addproduct),
 
     # Supplier url´s
     path('suppliers/', supplierlistview),
+    path('add-supplier/', addsupplier),
 ]
